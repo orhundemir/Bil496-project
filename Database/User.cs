159d0941b18b4e484ec6d9b3b96d85021c957151
Metin <mmetinkok@gmail.com>
2023-02-01 14:16:01 +0300

    User entity'si güncellendi
    Yapılan toplantıda alınan karar doğrultusunda entity dosyasına kullanıcının ismini ve oturum süresini tutmak için eklemeler yapıldı bu değişiklikler veri tabanı tablosuna eklenmesine gereksenim duyulmadığı için eklenmedi

--- Database/User.cs
+++ Database/User.cs
@@ -1,6 +1,8 @@
 public class User{
     int id;
     string e_mail;
+    string name; //Users name surname not in database but may be used for display will set from login
+    long sessionTime; //time variable set on starting  a session and may be used to clculate session duration for current session
     User(int id, string e_mail){
         this.id = id;
         this.e_mail = e_mail;
