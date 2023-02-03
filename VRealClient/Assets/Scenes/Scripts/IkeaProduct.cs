78c844759c89c8e4246e25c16f8fbca4546d2aea
Efe Karaman <efekaraman@hotmail.com>
2023-02-03 16:39:50 +0300

    IKEA API'den gelen veriyi Unity'ye aktaran scriptler commitlendi

---
+++ VRealClient/Assets/Scenes/Scripts/IkeaProduct.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Json objects sent from the Python script will be deserialized into objects of this class
+[System.Serializable]
+public class IkeaProduct {
+
+    public string productName;
+    public string productType;
+    public string productPrice;
+    public string productImageURL;
+    public string productMeasurements;
+
+    public override string ToString() {
+        return "name: " + productName + "\ntype: " + productType + "\nprice: " + productPrice + "\nimage: " + productImageURL + "\nmeasurements: " + productMeasurements;
+    }
+
+}
