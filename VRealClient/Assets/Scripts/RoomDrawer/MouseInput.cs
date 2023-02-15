71c321329f28018ae6fca67b228cb5a89d81fb95
Efe Karaman <efekaraman@hotmail.com>
2023-02-15 19:34:54 +0300

    Menteşeler kullanılarak çizim asisti sağlama özelliği eklendi

--- VRealClient/Assets/Scripts/RoomDrawer/MouseInput.cs
+++ VRealClient/Assets/Scripts/RoomDrawer/MouseInput.cs
@@ -24,12 +24,15 @@ public class MouseInput : MonoBehaviour
     // Detects the click and drag motion of the mouse and creates a wall object between it's start and end positions
     private void HandleMouseInputs()
     {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = Camera.main.transform.position.y;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
         // Instantiate the Wall Prefab at the mouse location when the left mouse button is first pressed
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePosition = Input.mousePosition;
-            mousePosition.z = Camera.main.transform.position.y;
-            clickPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            // If the mouse is clicked on top of a hinge object, set the starting position of the wall to its center
+            clickPosition = AdjustPositionForHinge(mousePosition);
 
             GameObject wall = wallHandler.CreateWall(clickPosition);
             previewWall = wall.GetComponent<WallObject>();
@@ -38,19 +41,41 @@ public class MouseInput : MonoBehaviour
         // Update the transparent preview wall object to follow the mouse position while the left mouse button is held down
         else if (Input.GetMouseButton(0))
         {
-            Vector3 mousePosition = Input.mousePosition;
-            mousePosition.z = Camera.main.transform.position.y;
-            Vector3 endPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-            wallHandler.UpdateWall(previewWall, clickPosition, endPosition);
+            wallHandler.UpdateWall(previewWall, clickPosition, mousePosition);
         }
 
-        // When the left mouse button is released, change the wall's material from transparent to opaque and stop updating that object
+        // When the left mouse button is released, stop updating the wall object
         if (Input.GetMouseButtonUp(0))
         {
+            // If the mouse is released on top of a hinge object, set the ending position of the wall to its center
+            Vector3 releasePosition = AdjustPositionForHinge(mousePosition);
+            wallHandler.UpdateWall(previewWall, clickPosition, releasePosition);
+
+            // Change its material from transparent to opaque and place hinges at both ends of it
             previewWall.ChangeMaterialToOpaque();
             wallHandler.AdjustHinges(previewWall);
         }
+    }
+
+    // Check if there is a hinge object at the given position and move the vector to its center
+    private Vector3 AdjustPositionForHinge(Vector3 position) {
+        GameObject hingeAtMousePosition = GetObjectByTagOnRaycastHit(Input.mousePosition, "Hinge");
+        if (hingeAtMousePosition != null) {
+            position.x = hingeAtMousePosition.transform.position.x;
+            position.z = hingeAtMousePosition.transform.position.z;
+        }
+
+        return position;
+    }
+
+    // Raycast from the main camera to the mouse position and return a hinge object if the ray hits one
+    private GameObject GetObjectByTagOnRaycastHit(Vector3 position, string tag) {
+        Ray ray = Camera.main.ScreenPointToRay(position);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+            if (hit.collider.gameObject.CompareTag(tag))
+                return hit.transform.gameObject;
+
+        return null;
     }
 
     // Checks if the given vector is contained inside the given collider
