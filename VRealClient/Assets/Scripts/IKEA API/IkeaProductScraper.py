06f245032b6e70b9b19ddfb5592db73cb65d7431
Efe Karaman <efekaraman@hotmail.com>
2023-03-05 01:46:15 +0300

    Yanlış ürün bilgilerinin getirilmesinin engellenmesi için bir kontrol daha eklendi
    URL'deki isim ve getirilen ürünün model adı karşılaştırılması eklendi. Kod düzenlenip daha anlaşılır hale getirildi.

--- VRealClient/Assets/Scripts/IKEA API/IkeaProductScraper.py
+++ VRealClient/Assets/Scripts/IKEA API/IkeaProductScraper.py
@@ -2,29 +2,46 @@ import ikea_api as ikea
 import json
 import sys
 
-def fetchProductInfo(productName):
+def fetchProductInfo(productURLName):
     """Retrieve product information from IKEA API based on the item name"""
-    # Run the API call and store the result
+    # Run the API call and store the search results
     constants = ikea.Constants(country = "us", language = "en")
     search = ikea.Search(constants)
-    endpoint = search.search(productName)
+    endpoint = search.search(productURLName)
     searchResult = ikea.run(endpoint)
     
-    # Extract the product information from the result
+    # Extract the product dictionary from the result
+    # Also check to see if the given product name is valid via a IndexOutOfBounds exception
+    # This exception is raised at the end of this script because of output redirection to Unity
     try:
-        product = searchResult['searchResultPage']['products']['main']['items'][0]['product']
+        product = searchResult["searchResultPage"]["products"]["main"]["items"][0]["product"]
     except IndexError:
-        notFoundProducts.append(productName)
+        notFoundProducts.append(productURLName)
         return None
 
     # Extract the desired information from the product dictionary
-    productName = product['name']
-    productType = product['typeName']
-    productPrice = str(product['salesPrice']['current']['prefix']) + str(product['salesPrice']['current']['wholeNumber']) + str(product['salesPrice']['current']['separator']) + str(product['salesPrice']['current']['decimals'])
-    productImageURL = product['mainImageUrl']
-    productMeasurements = product['itemMeasureReferenceText']
-    
-    return {"productName": productName, "productType": productType, "productPrice": productPrice, "productImageURL": productImageURL, "productMeasurements": productMeasurements}
+    productName = product["name"]
+    if productName.lower() not in productURLName.lower():
+        notFoundProducts.append(productURLName)
+        return None
+
+    productType = product["typeName"]
+    productPrice = (
+        str(product["salesPrice"]["current"]["prefix"])
+        + str(product["salesPrice"]["current"]["wholeNumber"])
+        + str(product["salesPrice"]["current"]["separator"])
+        + str(product["salesPrice"]["current"]["decimals"])
+    )
+    productImageURL = product["mainImageUrl"]
+    productMeasurements = product["itemMeasureReferenceText"]
+
+    return {
+        "productName": productName,
+        "productType": productType,
+        "productPrice": productPrice,
+        "productImageURL": productImageURL,
+        "productMeasurements": productMeasurements,
+    }
 
 class ProductNotFoundException(Exception):
     def __init__(self, failed_products):
@@ -45,6 +62,7 @@ productsJson = json.dumps(products)
 # The output of this print function will be redirected to Unity
 print(productsJson)
 
-# Send the exception message about the items that could not be fetched back to Unity
+# Send exception message for unfetched items back to Unity
+# This error has to be raised at the very end to avoid prematurely terminating and skipping remaining items
 if len(notFoundProducts) != 0:
     raise ProductNotFoundException(notFoundProducts)
