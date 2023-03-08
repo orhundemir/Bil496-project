9ff43099c8744dc717664630613628cb4c130422
Metin <mmetinkok@gmail.com>
2023-03-08 15:20:29 +0300

    Global npgsqlconnection object created
    For this object get/set methods and open/close connection methods created

--- VRealServer/Assets/Scripts/Database/DBManager.cs
+++ VRealServer/Assets/Scripts/Database/DBManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Npgsql;
 
 public class DBManager : MonoBehaviour
 {
     private static DBManager _singleton;
+
+    private NpgsqlConnection conn;
     public static DBManager Singleton
     {
         get => _singleton;
@@ -25,9 +28,23 @@ public class DBManager : MonoBehaviour
     {
         Singleton = this;
     }
-
-    //FIX ME: Connection kurmayý saðlayan obje Global deðiþken olmalýdýr.
-    //public DBConnection dbConn { get; set; }
+    //Public NpgsqlConnextion object set within the parameters inside the ConnectionManager class
+    public void setConnection(){
+        conn = ConnectionManager.getConnection();
+        openConnection();
+    }
+    //Returns NpgsqlConnection object
+    public NpgsqlConnection getConnection(){
+        return conn;
+    }
+    //Opens connection to the database
+    public void openConnection(){
+        conn.Open();
+    }
+    //Closes connection to the database too many idle connections can and will crash server
+    public void closeConnection(){
+        conn.Close();
+    }
     public Dictionary<ushort, Player> playerList { get; private set; }
 
     private void Start()
