cffcb1025fd62c7b1226a7383bee6b7dcf92bc61
Efe Karaman <efekaraman@hotmail.com>
2023-04-11 15:44:08 +0300

    Menü tuşu için listener eklendi, menünün kamerayı takip etmesi sağlandı

--- VRealClient/Assets/Scripts/VReal/MenuController.cs
+++ VRealClient/Assets/Scripts/VReal/MenuController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using HTC.UnityPlugin.Vive;
 
@@ -7,9 +5,23 @@ public class MenuController : MonoBehaviour
 {
 
     [SerializeField] private GameObject sceneMenuCanvas;
-    private Camera camera;
+    [SerializeField] public float distanceFromCamera = 1f;
 
-    void Start()
+    private new Camera camera;
+
+    // Assign listeners to controller buttons
+    private void Awake()
+    {
+        ViveInput.AddListenerEx(HandRole.RightHand, ControllerButton.Menu, ButtonEventType.Down, MenuButtonListener);
+    }
+
+    // Unassign the controller button listeners
+    private void OnDestroy()
+    {
+        ViveInput.RemoveListenerEx(HandRole.RightHand, ControllerButton.Trigger, ButtonEventType.Down, MenuButtonListener);
+    }
+
+    private void Start()
     {
         camera = FindObjectOfType<Camera>();
 
@@ -20,18 +32,15 @@ public class MenuController : MonoBehaviour
 
     private void Update()
     {
-        // Listens to the Menu button on the right hand controller
-        if (Input.GetKeyDown(KeyCode.Space) || ViveInput.GetPressDownEx(HandRole.RightHand, ControllerButton.Menu)) {
-            if (sceneMenuCanvas.active)
-                sceneMenuCanvas.SetActive(false);
-            else
-            {
-                sceneMenuCanvas.transform.position = camera.transform.position + camera.transform.forward;
-                sceneMenuCanvas.transform.rotation = Quaternion.LookRotation(camera.transform.forward);
-
-                sceneMenuCanvas.SetActive(true);
-            }
+        if (sceneMenuCanvas.active) {
+            sceneMenuCanvas.transform.position = camera.transform.position + camera.transform.forward * distanceFromCamera;
+            sceneMenuCanvas.transform.rotation = Quaternion.LookRotation(camera.transform.forward);
         }
+    }
+
+    private void MenuButtonListener()
+    {
+        sceneMenuCanvas.SetActive(!sceneMenuCanvas.active);
     }
 
 }
