8b939d85176173efef7aca401b54da16d428265c
Orhun Demir <orhundemir35@gmail.com>
2023-04-19 00:20:50 +0300

    VR headset tanıma ile ilgili değişiklikler

--- VRealClient/Assets/Scripts/VReal/Initiator.cs
+++ VRealClient/Assets/Scripts/VReal/Initiator.cs
@@ -2,13 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using HTC.UnityPlugin.VRModuleManagement;
+using UnityEngine.XR.Management;
+using UnityEngine.XR;
 
 public class Initiator : MonoBehaviour
 {
     [SerializeField] private GameObject RoomLight;
     void Awake()
     {
+
+        //XRGeneralSettings.Instance.Manager.InitializeLoaderSync();
+       // XRGeneralSettings.Instance.Manager.StartSubsystems();
+
         Player temp = Player.list[NetworkManager.Singleton.Client.Id];
+        VRModule.Initialize();
+        
+        //if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+        //{
+        //    Debug.LogWarning("Initializing XR Failed. Check Editor or Player log for details.");
+        //}
+        //else
+        //{
+        //    Debug.Log("Starting XR...");
+        //    XRGeneralSettings.Instance.Manager.StartSubsystems();
+        //}
+        
         Player player = Instantiate(GameLogic.Singleton.VROrigin, temp.transform.position, temp.transform.rotation).GetComponent<Player>();
         temp.CopyPlayer(player);
 
@@ -23,5 +42,5 @@ public class Initiator : MonoBehaviour
         lightPos.y = 2.5f;
         Instantiate(RoomLight, lightPos, Quaternion.identity);
     }
-
+    
 }
